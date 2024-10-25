4720fd3a9b300e0b3cae9b195de4a0afe8bb1ae0
May Gan <119158272+dis-may@users.noreply.github.com>
2024-10-25 22:54:59 +1300

    docs: make readme more legible and fix scripts

--- run.sh
+++ run.sh
@@ -4,21 +4,22 @@ if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
     # macOS or Linux
     echo "Starting python venv..."
     # start venv
-    source /venv/bin/activate
+    cd backend_mm
+    source venv/bin/activate
 
 
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
     # Windows (Git Bash or WSL)
     echo "Starting python venv..."
     # start venv
-    .venv\\Scripts\\activate
+    cd backend_mm
+    venv\\Scripts\\activate
 
 fi
 
 echo "python venv started"
 
 echo "starting backend"
-cd backend_mm
 python app.py &
 python chat.py &
 
--- setup.sh
+++ setup.sh
@@ -4,23 +4,24 @@ if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
     # macOS or Linux
     echo "Starting python venv..."
     # start venv
-    python -m venv .venv
-    source /venv/bin/activate
+    cd backend_mm
+    python3 -m venv venv
+    source venv/bin/activate
 
 
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
     # Windows (Git Bash or WSL)
     echo "Starting python venv..."
     # start venv
-    python -m venv .venv
-    .venv\\Scripts\\activate
+    cd backend_mm
+    python -m venv venv
+    venv\\Scripts\\activate
 
 fi
 
 echo "python venv started"
 
 echo "installing dependencies backend"
-cd backend_mm
 pip install -r requirements.txt
 
 echo "installing dependencies frontend"
